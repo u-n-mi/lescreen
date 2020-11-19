b9b09b0f1ee08d1aeb5c94d0dc58f81bcbc655b2
margarida santos <margarida.mimosa99@gmail.com>
2020-11-19 14:15:44 +0000

    seeds done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning Database'
+User.delete_all
+Interview.delete_all
+Video.delete_all
+Review.delete_all
 
+# users
+puts 'Creating users'
+maria = User.new(email: "maria@lescreen.com", password: "123456")
+maria.save!
 
-# user
 # Interview
+puts 'Creating Interviews'
+one = Interview.new(open_date: "19/11/2020", final_date: "25/11/2020", user: maria)
+one.save!
+
 # Videos
+puts 'Creating Videos'
+video_one = Video.new(name: "name1", interview: one)
+video_one.save!
+
 # Review
+puts 'Creating Reviews'
+review_one = Review.new(rating: "5", comment: "stars", video: video_one, user:maria)
+review_one.save!
+
+#done
+puts 'Its alright'
