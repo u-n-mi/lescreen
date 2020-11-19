ef4d610f237d5f9b7052b4a85ed9d3ed13923413
margarida santos <margarida.mimosa99@gmail.com>
2020-11-19 13:14:17 +0000

    can exit now

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  get 'dashboard', to: 'pages#dashboard'
   resources :interviews, only: [:index, :new, :create, :show] do
     resources :videos, only: [:index, :new, :create, :show] do
       resources :reviews, only: [:new, :create]
