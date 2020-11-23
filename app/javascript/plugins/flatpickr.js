a4de2b8daf2a81e8ad34586de312b0e1c4ec720c
Jonny Davis <jonny.alexander.davis@gmail.com>
2020-11-23 11:10:35 +0000

    datetimepicker

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -1,7 +1,11 @@
 import flatpickr from "flatpickr";
+import 'flatpickr/dist/themes/airbnb.css'
 
 const flat_pickr = () => {
-  flatpickr(".datepicker", {});
+  flatpickr(".datepicker", {
+    altInput: true,
+    enableTime: true
+  });
 }
 
 export { flat_pickr }
