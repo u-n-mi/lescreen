eb7aac0bfc4e0191605edf80d2f6c1d3c6c60023
Jonny Davis <jonny.alexander.davis@gmail.com>
2020-11-24 16:31:02 +0000

    sms autoform + notice

--- app/services/send_sms.rb
+++ app/services/send_sms.rb
@@ -1,13 +1,15 @@
 require 'twilio-ruby'
 class SendSms
-  def initialize(number, link)
+def initialize(number, link, company, deadline)
     @number = number
     @link = link
+    @company = company
+    @deadline = deadline
   end
   def send_message
     account_sid = ENV["TWILIO_ACCOUNT_SID"]
     account_token = ENV["TWILIO_AUTH_TOKEN"]
-    body = "Hello There #{@link}"
+    body = "Congratulations, you recently applied for a role at #{@company} and have been selected to complete an initial screening interview using Screen App. Please follow the link below to record yourself answering some initial questions before #{@deadline}-#{@link}"
         @client = Twilio::REST::Client.new(account_sid, account_token)
         @client.messages.create(
         body: body,
