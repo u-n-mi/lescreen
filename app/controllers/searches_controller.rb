dc7d6aaf72b9043e30c14c914db0ea3fcd5087b1
margarida santos <margarida.mimosa99@gmail.com>
2020-11-24 14:12:51 +0000

    not complete

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -5,5 +5,10 @@ class SearchesController < ApplicationController
 
   def create
     @search = Search.create!(params[:search])
+    redirect_to @search
+  end
+
+  def show
+    @search = Search.find(params[:id])
   end
 end
